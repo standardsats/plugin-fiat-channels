9d32f669d8e9f3acccc8701f038d752ec36d9eca
Anton Gushcha <ncrashed@protonmail.com>
2021-11-28 17:53:11 +0300

    Parse rate value

--- src/main/scala/fr/acinq/fc/app/rate/RateOracle.scala
+++ src/main/scala/fr/acinq/fc/app/rate/RateOracle.scala
@@ -1,10 +1,17 @@
 package fr.acinq.fc.app.rate
 
 import akka.actor.Actor
+import akka.actor.Status
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.pattern.pipe
+import akka.util.ByteString
 import fr.acinq.eclair
 import fr.acinq.eclair._
-import akka.http.scaladsl.client.RequestBuilding.Get
 import grizzled.slf4j.Logging
+import spray.json.DefaultJsonProtocol._
 
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -18,12 +25,36 @@ class RateOracle(kit: eclair.Kit) extends Actor with Logging { me =>
 
   var lastRate: MilliSatoshi = 0L.msat
 
+  implicit val system = context.system
+  val http = Http(system)
+
+  // case class RatesArray(name: String)
+  // implicit val petFormat = jsonFormat1(Pet)
+
   override def receive: Receive = {
     case RateOracle.TickUpdateRate =>
       logger.info("Updating current fiat rate")
+      queryRates
 
-      def resp = Get("https://api-pub.bitfinex.com/v2/ticker/tBTCUSD")
-      logger.info(s"${resp}")
+    case HttpResponse(StatusCodes.OK, headers, entity, _) =>
+      entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
+        Unmarshal(body).to[List[Float]].pipeTo(self)
+      }
 
+    case values: List[_] =>
+      logger.info("Got response, body: " + values)
+
+    case resp @ HttpResponse(code, _, _, _) =>
+      logger.info("Request failed, response code: " + code)
+      resp.discardEntityBytes()
+
+    case Status.Failure(e) =>
+      logger.error("Request failed: " + e)
   }
+
+  private def queryRates: Unit = {
+    http.singleRequest(HttpRequest(uri = "https://api-pub.bitfinex.com/v2/ticker/tBTCUSD"))
+      .pipeTo(self)
+  }
+
 }
